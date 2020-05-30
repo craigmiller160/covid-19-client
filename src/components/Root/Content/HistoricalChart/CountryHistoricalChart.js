2e589d41f2c4f63a09f9ab6914f87dd71b0362ac
Craig Miller <craigmiller160@gmail.com>
2020-05-30 10:11:20 -0400

    Added loading to historical charts

--- src/components/Root/Content/HistoricalChart/CountryHistoricalChart.js
+++ src/components/Root/Content/HistoricalChart/CountryHistoricalChart.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import BaseHistoricalChart from './BaseHistoricalChart';
 import CountrySearch from '../Search/CountrySearch';
+import { useDispatch } from 'react-redux';
+import { loadCountryHistoricalData } from '../../../../store/countryData/actions';
+import useLoading from '../../../hooks/useLoading';
 
-const CountryHistoricalChart = () => (
+const Component = () => (
     <div>
         <CountrySearch />
         <BaseHistoricalChart />
     </div>
 );
+
+const CountryHistoricalChart = () => {
+    const dispatch = useDispatch();
+    const loader = () => dispatch(loadCountryHistoricalData());
+    const DisplayComponent = useLoading({
+        loader,
+        component: Component
+    });
+
+    return (
+        <DisplayComponent />
+    );
+};
 
 export default CountryHistoricalChart;
--- src/components/Root/Content/HistoricalChart/StateHistoricalChart.js
+++ src/components/Root/Content/HistoricalChart/StateHistoricalChart.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import BaseHistoricalChart from './BaseHistoricalChart';
 import StateSearch from '../Search/StateSearch';
+import { useDispatch } from 'react-redux';
+import { loadStateHistoricalData } from '../../../../store/stateData/actions';
+import useLoading from '../../../hooks/useLoading';
 
-const StateHistoricalChart = () => (
+const Component = () => (
     <div>
         <StateSearch />
         <BaseHistoricalChart isState />
     </div>
 );
+
+const StateHistoricalChart = () => {
+    const dispatch = useDispatch();
+    const loader = () => dispatch(loadStateHistoricalData());
+    const DisplayComponent = useLoading({
+        loader,
+        component: Component
+    });
+
+    return (
+        <DisplayComponent />
+    );
+};
 
 export default StateHistoricalChart;

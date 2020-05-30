f0c3c68d3ff5dd3575f7233a30a3bf691b6f4794
Craig Miller <craigmiller160@gmail.com>
2020-05-30 13:28:36 -0400

    Finished implementing all menu items

--- src/components/Root/NavBar/menuItems.js
+++ src/components/Root/NavBar/menuItems.js
@@ -1,12 +1,3 @@
-import displaySlice, {
-    SECTION_COUNTRY_COMPARE_DATA,
-    SECTION_COUNTRY_HIST_CHARTS,
-    SECTION_COUNTRY_HIST_DATA,
-    SECTION_STATE_COMPARE_DATA,
-    SECTION_STATE_HIST_CHARTS,
-    SECTION_STATE_HIST_DATA
-} from '../../../store/display/slice';
-
 const menuItemCreator = (dispatch, pathname, closeMenu) => ({ label, to, exact = false }) => {
     const active = exact ? to === pathname : pathname !== '/' && to.startsWith(pathname);
     return {
@@ -34,13 +25,24 @@ const createMenuItems = (dispatch, pathname, closeMenu) => {
         createMenuItem({
             label: 'Country History Charts',
             to: '/country/history/chart'
+        }),
+        createMenuItem({
+            label: 'State History Data',
+            to: '/state/history/data',
+            exact: true
+        }),
+        createMenuItem({
+            label: 'State History Charts',
+            to: '/state/history/chart'
+        }),
+        createMenuItem({
+            label: 'Compare Country Data',
+            to: '/country/compare'
+        }),
+        createMenuItem({
+            label: 'Compare State Data',
+            to: '/state/compare'
         })
-        // createMenuItem(SECTION_COUNTRY_HIST_DATA, 'Country History Data', ),
-        // createMenuItem(SECTION_COUNTRY_HIST_CHARTS, 'Country History Charts'),
-        // createMenuItem(SECTION_STATE_HIST_DATA, 'State History Data'),
-        // createMenuItem(SECTION_STATE_HIST_CHARTS, 'State History Charts'),
-        // createMenuItem(SECTION_COUNTRY_COMPARE_DATA, 'Compare Country Data'),
-        // createMenuItem(SECTION_STATE_COMPARE_DATA, 'Compare State Data')
     ];
 
     const right = [];
